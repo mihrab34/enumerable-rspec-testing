e3c566e3990bdeb7a6c8171ad10f0d3d4ef0179b
Rahmat <akintolarahmah@gmail.com>
2020-01-24 16:26:09 +0000

    test added for myAll?, myAny?, myCount

--- spec/enum_spec.rb
+++ spec/enum_spec.rb
@@ -34,7 +34,56 @@ RSpec.describe "Enumerable" do
         it "returns elements of an array if block condition is passed " do
             expect([2, 'ant', 3, 'boat', 4, 'cat'].my_select { |item| item.class == String }).to  eql(%w[ant boat cat])
         end
+
+        it "returns enumerator if no block is given" do
+            expect([1, 2, 3].my_select).to  be_a(Enumerator)
+        end 
+    end
+
+    describe "#my_all?" do
+        it "returns true if the block never return false or nil" do
+            expect(%w[ant, bat, cat, mat ].my_all? { |item| item.size >= 3 }).to  eql(true)
+        end
+
+        it "returns true if no block given" do
+            expect(%w[ant, bat, cat, mat ].my_all?).to  eql(true)
+        end
+    end
+
+    describe "#my_any?" do
+        it "returns true if the block ever return a value other than false or nil" do
+            expect(%w[ant, bat, cat, matter, mouse, sit ].my_any? { |item| item.size <= 3 }).to  eql(true)
+        end
+
+        it "returns true if no block given" do
+            expect(%w[ant, bat, cat, matter, mouse, sit ].my_any?).to  eql(true)
+        end
+    end
+    
+    describe "#my_none?" do
+        it "returns true if the block never returns true for all the elements of the array " do
+            expect([2, 4, 6, 8, 10].my_none?(&:odd?)).to  eql(true)
+        end
+
+        it "returns true if none of the array elements are true and no block given" do
+            expect([2, 4, 6, 8, 10].my_none?(&:even?)).to  eql(false)
+        end
+    end
+
+    describe "#my_count" do
+        it "returns the number of element yielding a true value" do
+            expect([1, 2, 3, 4, 5, 8].my_count(&:odd?)).to  eql(3)
+        end
         
+        it "returns the number of occurence of the argument given in array" do
+            expect([1, 2, 3, 1, 1, 5, 4, 1].my_count(1)).to  eql(4)  
+        end
+        
+        it "returns the number of elements of an array" do
+            expect([1, 2, 3, 4, 5, 8].my_count).to  eql(6)
+        end
+        
+
     end
     
     
